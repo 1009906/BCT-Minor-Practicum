8d03d5394ccad4665698db0ea861b940f15bb3dc
1009906 <1009906@hr.nl>
2023-12-14 15:28:50 +0100

    Changed context and added new clientV3

--- Assignment/Goodchain/src/system/context.py
+++ Assignment/Goodchain/src/system/context.py
@@ -18,12 +18,28 @@ class Context:
     # temp_pool_path = "data/pool/temppooltx.dat"
     # hash_file_path = "data/hash_file.txt"
 
+    #All used paths, depends on current_node
     database_path = f"data/{current_node}/database.db"
     ledger_path = f"data/{current_node}/ledger.dat"
     temp_ledger_path = f"data/{current_node}/templedger.dat"
     pool_path = f"data/{current_node}/pooltx.dat"
     temp_pool_path = f"data/{current_node}/temppooltx.dat"
     hash_file_path = f"data/{current_node}/hash_file.txt"
+
+    #All used ports
+    HOST_IP = 'localhost'
+    
+    W_SERVER_PORTS = [5000, 5002]
+    M_SERVER_PORTS = [5001, 5003]
+
+    W_SERVER_PORT = 5000 if current_node == "node1" else 5002
+    M_SERVER_PORT = 5001 if current_node == "node1" else 5003
+
+    # W_SERVER_PORT_CURRENT_NODE = 5000
+    # W_SERVER_PORT_OTHER_NODE = 5002
+
+    # M_SERVER_PORT_CURRENT_NODE = 5001
+    # M_SERVER_PORT_OTHER_NODE = 5003
     
     def __repr__(self):
         repr_str = f"User id: {self.user_id}\n"
--- Testfiles/Networking/clientV3.py
+++ Testfiles/Networking/clientV3.py
@@ -9,97 +9,111 @@ from cryptography.hazmat.primitives import serialization
 
 HEADER = 64
 PORT = 5050
+W_SERVER_PORTS = [5000, 5002]
+
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
 HOST_IP = 'localhost'
 
 ADDR = (HOST_IP, PORT)
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(ADDR)
-client_name = input('please enter your name: ')
-client.send(client_name.encode(FORMAT))
-print(client.recv(2048).decode(FORMAT))
-
-def send(msg):
-    message = msg.encode(FORMAT)
-    msg_length = len(message)
-    send_length = str(msg_length).encode(FORMAT)
-    send_length += b' ' * (HEADER - len(send_length))
-    client.send(send_length)
-    client.send(message)
-    print(client.recv(2048).decode(FORMAT))
-
-def send_tx(tx):
-    # Serialize the Tx object before sending it
-    serialized_tx = pickle.dumps(tx)
-    msg_length = len(serialized_tx)
-    send_length = str(msg_length).encode(FORMAT)
-    send_length += b' ' * (HEADER - len(send_length))
-    client.send(send_length)
-    client.send(serialized_tx)
-    print(client.recv(2048).decode(FORMAT))
-
-def stop_the_client():
-    global cont_flag
-    mes = DISCONNECT_MESSAGE
-    send(mes)
+class WalletClient:
+    client = None
     cont_flag = False
 
-def create_test_transaction():
-    alex_prv, alex_pbc = generate_keys()
-    mike_prv, mike_pbc = generate_keys()
+    def __init__(self):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client.connect(ADDR)
+        client_name = input('please enter your name: ') #TODO take username from context.
+        self.client.send(client_name.encode(FORMAT))
+        print(self.client.recv(2048).decode(FORMAT))
+
+    def send(self, msg):
+        message = msg.encode(FORMAT)
+        msg_length = len(message)
+        send_length = str(msg_length).encode(FORMAT)
+        send_length += b' ' * (HEADER - len(send_length))
+        self.client.send(send_length)
+        self.client.send(message)
+        print(self.client.recv(2048).decode(FORMAT))
+
+    def send_tx(self, tx):
+        # Serialize the Tx object before sending it
+        serialized_tx = pickle.dumps(tx)
+        msg_length = len(serialized_tx)
+        send_length = str(msg_length).encode(FORMAT)
+        send_length += b' ' * (HEADER - len(send_length))
+        self.client.send(send_length)
+        self.client.send(serialized_tx)
+        print(self.client.recv(2048).decode(FORMAT))
+
+    def stop_the_client(self):
+        # global cont_flag
+        mes = DISCONNECT_MESSAGE
+        self.send(mes)
+        self.cont_flag = False
+        self.client.close() #TODO: check if this is needed.
+
+    def create_test_transaction(self):
+        alex_prv, alex_pbc = generate_keys()
+        mike_prv, mike_pbc = generate_keys()
 
 
-    alex_prv_ser = alex_prv.private_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PrivateFormat.PKCS8,
-        encryption_algorithm=serialization.NoEncryption()
-        )
-    
-    alex_pbc_ser = alex_pbc.public_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PublicFormat.SubjectPublicKeyInfo
-        )
-    
-    mike_pbc_ser = mike_pbc.public_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PublicFormat.SubjectPublicKeyInfo
-        )
+        alex_prv_ser = alex_prv.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.PKCS8,
+            encryption_algorithm=serialization.NoEncryption()
+            )
+        
+        alex_pbc_ser = alex_pbc.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
+            )
+        
+        mike_pbc_ser = mike_pbc.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
+            )
 
-    Tx1 = Tx(uuid.uuid1(), "Alex", "Mike", 0.1)
-    Tx1.add_input(alex_pbc_ser, 2.3)
-    Tx1.add_output(mike_pbc_ser, 1.0)
-    Tx1.sign(alex_prv_ser)
+        Tx1 = Tx(uuid.uuid1(), "Alex", "Mike", 0.1)
+        Tx1.add_input(alex_pbc_ser, 2.3)
+        Tx1.add_output(mike_pbc_ser, 1.0)
+        Tx1.sign(alex_prv_ser)
 
-    return Tx1
+        return Tx1
 
-cont_flag = True
-while cont_flag:
+    def handle_server(self): 
+        self.cont_flag = True
+        while self.cont_flag:
 
-    timeout = 15
-    timeout_thread = Timer(timeout, stop_the_client, [])
-    timeout_thread.start()
+            timeout = 15
+            timeout_thread = Timer(timeout, self.stop_the_client, [])
+            timeout_thread.start()
 
-    if not cont_flag:
-        print('The connection has already terminated.')
-        break
+            if not self.cont_flag:
+                print('The connection has already terminated.')
+                break
 
-    print('--------------------------')
-    print('You can send a message to server')
-    print('to stop connection, press enter on a blank message')
-    mes = input('Your message: ')
-    try:
-        if not mes:
-            mes = DISCONNECT_MESSAGE
-            cont_flag = False
-            send(mes)
-        else:
-            # send(mes)
-            tx = create_test_transaction()
-            send_tx(tx)
-    except:
-        print('The connection has already terminated.')
-        cont_flag = False
+            print('--------------------------')
+            print('You can send a message to server')
+            print('to stop connection, press enter on a blank message')
+            mes = input('Your message: ')
+            try:
+                if not mes:
+                    # mes = DISCONNECT_MESSAGE
+                    # self.cont_flag = False
+                    # self.send(mes)
+                    self.stop_the_client()
+                else:
+                    # send(mes)
+                    tx = self.create_test_transaction()
+                    self.send_tx(tx)
+                    self.stop_the_client()
+            except:
+                print('The connection has already terminated.')
+                self.cont_flag = False
 
-    timeout_thread.cancel()
+            timeout_thread.cancel()
+
+wallet_client = WalletClient()
+wallet_client.handle_server()
